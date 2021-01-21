3fabbcb7c242faa69eb18c1663a80fe8ef162a82
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-01-21 15:47:00 +0100

    Exo cherdepute finalisé

--- lib/03_cherdepute.rb
+++ lib/03_cherdepute.rb
@@ -1,18 +1,25 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-list = []
+a = []
+
 def profile(url)
+begin
 	page = Nokogiri::HTML(URI.open(url))
 	identity = {first_name: page.xpath("/html/body/div/div[3]/div/div/div/section[1]/div/article/div[2]/h1").text.split[1], 
 	last_name: page.xpath("/html/body/div/div[3]/div/div/div/section[1]/div/article/div[2]/h1").text.split[2..].join(" "), 
 	email: page.xpath("/html/body/div/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li[2]/a").text}
-return identity
+	return identity
+	rescue RuntimeError
+		return {error: "Data not found"}
+	end
 end
 
+page = Nokogiri::HTML(URI.open("https://www2.assemblee-nationale.fr/deputes/liste/alphabetique"))
+liens = page.css("#deputes-list").css("a")
+	
+liens.each_with_index do |lien, position|
+a << profile("https://www2.assemblee-nationale.fr#{(liens[position]["href"])}")
+end
 
-
-
-
-list << profile(url)
-
+puts a
---
+++ lib/test2.rb
@@ -0,0 +1,3 @@
+var = "Mme. Jean de la fontaine"
+puts var.split[1]
+puts var.split[2..].join(" ")
